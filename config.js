f69338e71b6e72bfcf9dad0635218adffb150917
Ashar Khan <asharullah.khan2468@gmail.com>
2024-07-31 21:01:34 +0500

    todo app with firestore

--- config.js
+++ config.js
@@ -7,17 +7,16 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebas
 
 //Firestore cdns
 
+ const firebaseConfig = {
+   apiKey: "AIzaSyCxVDP6qSZt5e3qdAyKYEf5YwFa4HFsHtM",
+   authDomain: "fir-authentication-956b7.firebaseapp.com",
+   projectId: "fir-authentication-956b7",
+   storageBucket: "fir-authentication-956b7.appspot.com",
+   messagingSenderId: "556321944730",
+   appId: "1:556321944730:web:f2524902c7a708f7565b23",
+   measurementId: "G-FHLZP4ZN7C",
+ };
 
-
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
